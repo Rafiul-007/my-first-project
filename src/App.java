25415a372a9d2781ab58f566eb6ecf8b5f946884
unknown <Rafiul-007>
2023-05-20 12:29:49 +0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(5*5);
+        double myNum= 10.50;
+        int book= (int)myNum;
+        System.out.println(book);
+    }
+}
---
+++ src/callingMethod/Main.java
@@ -0,0 +1,15 @@
+package callingMethod;
+
+public class Main {
+    static void myMethod (String fname)
+        {
+           System.out.println(fname+ " JOBS");
+        }
+        public static void main(String[]args)
+       {
+           myMethod("steve");
+           myMethod("liam");
+       }
+   }
+   
+
